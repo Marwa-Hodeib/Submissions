5178c72b086d28cff2815eba4a3b321ffad20e29
marwa-hodeib <marwahodeib31@gmail.com>
2019-12-16 15:09:20 +0200

    Step_6.js finished

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,4 @@
+var first_number = prompt("Enter first number :");
+var second_number = prompt("Enter second number :");
+var validate = first_number * second_number;
+alert("Your Answer is : " + first_number / second_number);
