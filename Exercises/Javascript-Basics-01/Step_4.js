d9360a1424a9c01b19c04514a78ce279a08ecd73
marwa-hodeib <marwahodeib31@gmail.com>
2019-12-16 14:34:13 +0200

    Step_4.js finished

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("Enter your name : ", "your name here");
+var surname = prompt("Enter your surname : ", "your surname");
+var city = prompt("Enter your city : ", "your city name");
+alert("Name: " + name + "\n surname : " + surname + "\n city :");
