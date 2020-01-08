897ce54d78ddabf6edee29dbd532f2318c20903f
marwa-hodeib <marwahodeib31@gmail.com>
2020-01-08 15:37:10 +0200

    finished step 1

---
+++ Exercises/Javascript-Basics-03/step 1/step 1.js
@@ -0,0 +1,7 @@
+var image1 = document.querySelector("#image1");
+image1.onmouseover = function() {
+  image1.style.border = "3px solid red";
+};
+image1.onmouseleave = function() {
+  image1.style.border = "none";
+};
