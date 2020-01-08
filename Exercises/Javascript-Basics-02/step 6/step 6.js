1879d5cfd0c5aecc9c2fbf58286cdc63f0401477
marwa-hodeib <marwahodeib31@gmail.com>
2020-01-08 12:44:17 +0200

    finished step 6

---
+++ Exercises/Javascript-Basics-02/step 6/step 6.js
@@ -0,0 +1,43 @@
+img1 = document.querySelector("#image1");
+img1.onmouseover = function() {
+  img1.setAttribute("src", "images/image1_2.jpg");
+};
+
+img1.onmouseleave = function() {
+  img1.setAttribute("src", "images/image1.jpg");
+};
+
+img2 = document.querySelector("#image2");
+img2.onmouseover = function() {
+  img2.setAttribute("src", "images/image2_2.jpg");
+};
+
+img2.onmouseleave = function() {
+  img2.setAttribute("src", "images/image2.jpg");
+};
+img3 = document.querySelector("#image3");
+img3.onmouseover = function() {
+  img3.setAttribute("src", "images/image3_2.jpg");
+};
+
+img3.onmouseleave = function() {
+  img3.setAttribute("src", "images/image3.jpg");
+};
+
+img4 = document.querySelector("#image4");
+img4.onmouseover = function() {
+  img4.setAttribute("src", "images/image4_2.jpg");
+};
+
+img4.onmouseleave = function() {
+  img4.setAttribute("src", "images/image4.jpg");
+};
+
+img5 = document.querySelector("#image5");
+img5.onmouseover = function() {
+  img5.setAttribute("src", "images/image5_2.jpg");
+};
+
+img5.onmouseleave = function() {
+  img5.setAttribute("src", "images/image5.jpg");
+};
