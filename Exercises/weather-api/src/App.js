5d37671b41ca44157c32532adbbee3f414dc0425
marwa-hodeib <marwahodeib31@gmail.com>
2020-01-16 14:32:02 +0200

    Weather API: Getting the LIVE weather data

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -9,20 +9,50 @@ import WeatherLater from "./WeatherLater";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      result: null,
+      city: "India"
+    };
+  }
+  async ApiCaller(param) {
+    const api =
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      this.state.city +
+      "&cnt=8&units=metric&appid=571df88d447302a15fda544e0999ffbe";
+    const response = await fetch(api);
+    const r = await response.json();
+    this.setState({
+      result: r
+    });
+  }
+  callbackFunction = childData => {
+    this.setState({ city: childData });
+    this.ApiCaller(childData);
+  };
+
+  async componentDidMount() {
+    this.ApiCaller(this.state.city);
   }
 
   render() {
     return (
       <div className="app">
-        {console.log(FakeWeather.list[0].main.humidity)}
-        <Search />
-        <CurrentWeather hum={FakeWeather.list[0].main.humidity} />
-        <div className="forecast">
-          {FakeWeather.list.map((item, value) => {
-            if (value > 0 && value < 8) return <WeatherLater weather={item} />;
-          })}
-        </div>
+        <Search parentCallback={this.callbackFunction} />
+        {this.state.result !== null ? (
+          <CurrentWeather hum={this.state.result.list[0].main.humidity} />
+        ) : (
+          "loading"
+        )}
+        {this.state.result !== null ? (
+          <div className="forecast">
+            {this.state.result.list.map((item, value) => {
+              if (value > 0 && value < 8)
+                return <WeatherLater weather_list={item} />;
+            })}
+          </div>
+        ) : (
+          "loading"
+        )}
 
         <main className="appmain"></main>
       </div>
--- Exercises/weather-api/src/Search.js
+++ Exercises/weather-api/src/Search.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    searchInput: "India"
+  };
+  sendData = () => {
+    this.props.parentCallback(this.state.searchInput);
+  };
   render() {
     return (
-      <header className="appheader">
-        <form className="whattemperature">
-          <input className="cityname" type="text" placeholder="Write City" />
-          <button type="submit" value="submit">
+      <>
+        <div className="appheader">
+          <input
+            type="text"
+            onChange={event => {
+              this.setState({
+                searchInput: event.target.value
+              });
+            }}
+            className="cityname"
+            placeholder="app_main"
+            value={this.state.searchInput}
+          ></input>
+          <button
+            type="submit"
+            onClick={event => {
+              this.sendData(this.state.searchInput);
+            }}
+            value="FIND WEATHER"
+            className="find_button"
+          >
             Find weather
           </button>
-        </form>
-      </header>
+        </div>
+      </>
     );
   }
 }
-
 export default Search;
