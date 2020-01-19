b915274fbb145f85516506dc436d7038649e2d72
marwa-hodeib <marwahodeib31@gmail.com>
2020-01-19 11:34:08 +0200

    Basic Queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,21 @@
+1-SELECT name FROM students
+
+2-SELECT * FROM students WHERE Age > 30
+
+3-SELECT name FROM students WHERE Gender = "F" AND Age = 30
+
+4-SELECT Points FROM students WHERE name="Alex"
+
+5-UPDATE students
+SET Gender = 'F' , Points = '95'
+WHERE name = 'Marwa';
+
+6-UPDATE students
+SET Points = '400'
+WHERE name = 'Basma';
+
+7-UPDATE students
+SET Points = '190'
+WHERE name = 'Alex';
+
+
