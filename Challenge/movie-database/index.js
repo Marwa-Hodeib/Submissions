8d71ba0c33a9c9a4ff768f31c18d300636744511
marwa-hodeib <marwahodeib31@gmail.com>
2020-01-22 16:52:32 +0200

    step3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,10 +5,20 @@ app.get("/", function(req, res) {
   res.send("hello world");
 });
 
-app.listen(5050, () => {
-  console.log("hey");
-});
+app.listen(5050);
 
 app.get("/test", (req, res) => {
   res.json({ status: 200, message: "ok" });
+});
+
+app.get("/time", (req, res) => {
+  res.json({
+    status: 200,
+    message:
+      new Date().getHours() +
+      ":" +
+      new Date().getMinutes() +
+      ":" +
+      new Date().getSeconds()
+  });
 });
