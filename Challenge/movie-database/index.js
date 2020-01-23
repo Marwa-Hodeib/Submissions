37a860bf3d39e9b3108a1e13eac24546da008329
marwa-hodeib <marwahodeib31@gmail.com>
2020-01-23 10:59:34 +0200

    step5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 }
+];
 
 app.get("/", function(req, res) {
   res.send("hello world");
@@ -26,7 +32,7 @@ app.get("/time", (req, res) => {
 app.get("/hello/:ID", (req, res) => {
   res.json({
     status: 200,
-    message: "hello" + req.params.ID
+    message: "hello " + req.params.ID
   });
 });
 
@@ -39,4 +45,20 @@ app.get("/search", (req, res) => {
       message: "you have to provide a search"
     });
   }
+});
+
+app.get("/movie/get", (req, res) => {
+  res.json({ status: 200, data: movies });
+});
+
+app.get("/movie/add", (req, res) => {
+  res.json({ status: 200, data: movies });
+});
+
+app.get("/movie/edit", (req, res) => {
+  res.json({ status: 200, data: movies });
+});
+
+app.get("/movie/delete", (req, res) => {
+  res.json({ status: 200, data: movies });
 });
